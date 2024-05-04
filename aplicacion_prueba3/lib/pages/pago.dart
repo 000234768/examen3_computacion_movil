20a835ccfdabbe2b4ddbc33b71fe764477bdcb38
Sergio Hernández <sahy08@hotmail.com>
2024-05-04 01:38:26 -0400

    exportación de páginas a pago.dart

---
+++ aplicacion_prueba3/lib/pages/pago.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PaginaPago extends StatelessWidget {
+  const PaginaPago({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      );
+  }
+}
--- aplicacion_prueba3/lib/widgets/widgets.dart
+++ aplicacion_prueba3/lib/widgets/widgets.dart
@@ -1,4 +1,3 @@
 // export 'package:flutter_application_1/widgets/card_container.dart';
-
 // export 'package:flutter_application_1/widgets/auth_background_c1.dart';
 // export 'package:flutter_application_1/widgets/auth_background.dart';
