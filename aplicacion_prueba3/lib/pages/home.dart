910f10526e5aea0ef93872eb60a8d3e415c69dd9
Agustín Ignacio Fernández Toledo <afernandez@mq.cl>
2024-05-04 20:57:28 -0400

    obtain data

--- aplicacion_prueba3/lib/pages/home.dart
+++ aplicacion_prueba3/lib/pages/home.dart
@@ -1,29 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application_1/widgets/header.dart';
 import 'package:flutter_application_1/widgets/footer.dart';
+import 'package:flutter_application_1/widgets/card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyHeader(), // Usa el header
+      appBar: MyHeader(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                'Aquí está el body',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Tienda online',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          MyFooter(), // Usa el footer
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Nuestros Productos',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('productos')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                return GridView.count(
+                    crossAxisCount: 2,
+                    childAspectRatio:
+                        0.7, // Ajusta este valor según tus necesidades
+                    children: documents.map((doc) {
+                      final Map<String, dynamic> data =
+                          doc.data() as Map<String, dynamic>;
+                      return CustomCard(
+                        title: data['Nombre'] ?? 'Sin nombre',
+                        price: data['Costo'] ?? 'Sin descripción',
+                        Url: data['Imagen'] ?? 'Sin imagen',
+                        buttonText: 'Agregar al carrito',
+                        onPress: () {},
+                      );
+                    }).toList(),
+                );
+              },
+            ),
+          ),
+          MyFooter(),
         ],
       ),
     );
---
+++ aplicacion_prueba3/lib/widgets/card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final double price;
+  final String buttonText;
+  final VoidCallback onPress;
+  final String Url;
+
+  const CustomCard({
+    Key? key,
+    required this.title,
+    required this.price,
+    required this.buttonText,
+    required this.onPress,
+    required this.Url
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: onPress,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.network(
+                Url,
+              ),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'USD ${price.toStringAsFixed(2)}', // Format price as CLP
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              TextButton(onPressed: onPress, child: Text(buttonText))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
