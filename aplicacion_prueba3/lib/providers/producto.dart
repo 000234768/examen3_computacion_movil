c6784addfeadc82c8d04d8a5c11082e6f4eed3a2
000234768 <juan_lulavel@hotmail.com>
2024-05-04 22:55:18 -0400

    cambios

---
+++ aplicacion_prueba3/lib/providers/producto.dart
@@ -0,0 +1,11 @@
+class Producto {
+  final String nombre;
+  final String imagen;
+  final double costo;
+
+  Producto({
+    required this.nombre,
+    required this.imagen,
+    required this.costo,
+  });
+}
